5626e45176632fc60eb37ec4c90fd11d2d947537
Mart <numen365258@gmail.com>
2026-04-27 19:54:08 +0200

    Remove duplicate nested project folder

--- reversible-computing-playground/src/revcomp/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Reversible Computing Playground."""
-
-from .gates import AND, CNOT, FREDKIN, NOT, OR, TOFFOLI, XOR, is_reversible, truth_table
-from .landauer import energy_per_erased_bit, total_erasure_energy
-
-__all__ = [
-    "AND", "CNOT", "FREDKIN", "NOT", "OR", "TOFFOLI", "XOR",
-    "is_reversible", "truth_table", "energy_per_erased_bit", "total_erasure_energy",
-]
